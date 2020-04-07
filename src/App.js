f49d651f8464e93dabe82657f6cd9f26eb9a4b25
Alexis Mendez <alexis.mendezpvd@gmail.com>
2020-04-07 11:51:01 -0400

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  
+  render() { 
   return (
     <div className="App">
-    
+      <h1>Welcome to BrainHive</h1>
     </div>
-  );
+   );
+  }
 }
 
 export default App;
