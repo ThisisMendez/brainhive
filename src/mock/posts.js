46f48450a196b0a7f0b8dccb2584653df50e24a0
Alexis Mendez <alexis.mendezpvd@gmail.com>
2020-04-13 11:59:36 -0400

    added mock file

---
+++ src/mock/posts.js
@@ -0,0 +1,52 @@
+
+const posts = [{
+    posterName: 'Arnell Milhouse',
+    resourceAuthor: 'Albert Einstein',
+    jobSkillLevel: 'Advanced',
+    cohort: '0',
+    title: 'The Universal Equation',
+    categories: ['FORTRAN', 'redux'],
+    summary: "This was a great article about blending fortran and redux.",
+    link: 'www.google.com/search?q=Albert%20Einstein',
+    resourceType: 'Article',
+    datePublished: '1943-10-03.00:00:00z',
+    videoLength: null,
+    timeToComplete: 3,
+    rating: 5,
+    comments: [{commenter: "Cliff", text: 'Nice post'}]
+  }, 
+  {
+    posterName: 'Tom Jones',
+    resourceAuthor: 'Kanye West',
+    jobSkillLevel: 'Advanced',
+    cohort: '9',
+    title: 'Follow Javascript',
+    categories: ['javascript'],
+    summary: "CSS is cool, but Javascript is the best language",
+    link: 'https://www.youtube.com/watch?v=ivCY3Ec4iaU',
+    resourceType: 'Video',
+    datePublished: '2019-10-03.00:00:00z',
+    videoLength: "3 min",
+    timeToComplete: 3,
+    rating: 5,
+    comments: [{commenter: "Alex", text: 'Fantastic post'}]
+  }, 
+  {
+    posterName: 'Jon Jones',
+    resourceAuthor: 'Bill Gates',
+    jobSkillLevel: 'Advanced',
+    cohort: '9',
+    title: 'HTML Stuff',
+    categories: ['HTML'],
+    summary: "HTML is the best",
+    link: 'https://www.w3schools.com/html/',
+    resourceType: 'Article',
+    datePublished: '2019-10-03.00:00:00z',
+    videoLength: null,
+    timeToComplete: 3,
+    rating: 5,
+    comments: [{commenter: "Tom", text: 'Great post'}]
+  }]
+
+
+export default posts
